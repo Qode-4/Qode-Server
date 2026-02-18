c5a3e5630333a41a9d775605379011e5f62079cb
ryu-hyesu <aimee3994@naver.com>
2026-02-18 12:34:06 +0900

    feat: chat types 구성

---
+++ src/modules/chat/chat.types.ts
@@ -0,0 +1,25 @@
+export interface Messages {
+  id: string; // PK
+  chat_id: string; // FK 
+  user_id: string; // FK 
+  content: string; 
+  message_status: 'COMPLETE' | 'PENDING' | 'FAILED';
+  created_at: string;
+}
+
+export interface Chats {
+  id: string; // PK
+  project_id: string; // FK
+  created_by: string; // FK
+  name: string;
+  chat_type: 'PERSONAL' | 'TEAM';
+  created_at: string;
+}
+
+export interface ChatParticipants {
+  chat_id: string; // PK
+  user_id: string; // PK
+  member_role: 'OWNER' | 'ADMIN' | 'MEMBER'; // default 'MEMBER'
+  created_at: string;
+  left_at: string | null; // TODO: 채팅 나가기 기능
+}
